1177ed1befc00678999c8f991fd6d13f209ead7b
Carlos Balacuit <Carlos.Balacuit@jpl.nasa.gov>
2018-11-09 23:14:21 -0800

    Added definitions and skills

---
+++ src/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import datetime
+import json
+import yaml
+
+def default(o):
+  if type(o) is datetime.date or type(o) is datetime.datetime:
+    return o.strftime("%Y-%b-%d")
+
+file_name = "../data/definitions"
+file_name = "../data/skills"
+#file_name = "resume.yaml"
+with open(file_name, "r") as f:
+    dataMap = yaml.safe_load(f)
+
+print(json.dumps(dataMap, indent=2, default=default))
