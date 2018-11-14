f64b163f83f9fa80422ecf57badb9381a6d36f58
Carlos Balacuit <Carlos.Balacuit@jpl.nasa.gov>
2018-11-14 06:58:06 -0800

    updated awards, companies, definitions, training

--- src/test.py
+++ src/test.py
@@ -4,14 +4,51 @@ import datetime
 import json
 import yaml
 
-def default(o):
+def coalesce(*arg):
+  for el in arg:
+    if el is not None:
+      return el
+  return None
+
+def datetime_formatter(o):
   if type(o) is datetime.date or type(o) is datetime.datetime:
     return o.strftime("%Y-%b-%d")
 
-file_name = "../data/definitions"
-file_name = "../data/companies"
-#file_name = "resume.yaml"
-with open(file_name, "r") as f:
-    dataMap = yaml.safe_load(f)
+def read_yaml(file):
+	with open("../data/" + file, "r") as f:
+		return yaml.safe_load(f)
 
-print(json.dumps(dataMap, indent=2, default=default))
+def dump(data):
+	json.dumps(data, indent=2, default=datetime_formatter)
+
+def report_awards():
+	data = read_yaml('awards')
+
+	print("## {}\n".format(data['title']))
+
+	awards = data['entries']
+	awards.sort(key=lambda i:i['dated'], reverse=True)
+
+	for award in awards:
+
+		type = award['type']
+		if award['type'] == 'Bonus Award': 	# be more specific
+			award['category']
+
+	 	print("{} ({}){}{}".format(
+	 			type,
+				award['dated'].strftime("%Y"),
+				'\t' * ((32 - len(type)) / 8),
+				award.get('summary', award.get('team', award.get('category')))
+	 		))
+
+def report_definitions():
+	definitions = read_yaml('definitions')
+
+
+
+
+report_definitions()
+
+report_awards()
+
